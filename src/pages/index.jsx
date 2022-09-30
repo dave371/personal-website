46a0da667f8ea840af85fe9a7b45d46b6eef81c8
BuildTools <unconfigured@null.spigotmc.org>
2022-09-30 19:19:01 -0400

    navbar and hero concept

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,52 +1,127 @@
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 function Home() {
   return (
-    <div className="p-2 bg-[#1B1B22]">
-      <Navbar />
+    <DefaultPageLayout>
       <Hero />
       <Portfolio />
       <Contact />
-    </div>
+    </DefaultPageLayout>
   );
 }
 export default Home;
 
-// single responsibility principle
-//a thing should only focus on ony thing
-function Navbar() {
+function DefaultPageLayout({ children }) {
+  const [toggleNav, setToggleNav] = useState(true);
+
+  useEffect(() => {
+    const hideOverflow = () => {
+      const body = document.querySelector('body');
+      body.style.overflow = toggleNav ? 'hidden' : 'auto';
+    };
+
+    hideOverflow();
+  }, [toggleNav]);
+
   return (
-    <nav className="flex justify-between items-center text-white">
-      <h1>David Pacheco</h1>
-      <ul className="flex gap-2">
-        <li>
-          <Link href="/">
-            <a>Home</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/portfolio">
-            <a>Portfolio</a>
-          </Link>
-        </li>
-        <li>
-          <Link href="/contact">
-            <a>Contact</a>
-          </Link>
-        </li>
-      </ul>
-    </nav>
+    <div className="relative min-h-screen px-5 bg-[#1B1B22] lg:w-[80%] lg:mx-auto">
+      <Navbar setToggleNav={setToggleNav} />
+      <div>{children}</div>
+
+      {toggleNav && <MobileNav setToggleNav={setToggleNav} />}
+    </div>
+  );
+}
+
+function Navbar({ setToggleNav }) {
+  return (
+    <header className="sticky top-0 w-full h-fit bg-[#1B1B22] py-2 text-white z-10">
+      <nav className="flex items-center justify-between">
+        <h1 className="text-2xl font-bold">David Pacheco</h1>
+        <FullNav />
+        <button
+          onClick={() => setToggleNav((prev) => !prev)}
+          className="p-2 bg-white text-[#DD1155] text-lg rounded font-medium md:hidden"
+        >
+          Menu
+        </button>
+      </nav>
+    </header>
+  );
+}
+
+function FullNav() {
+  return (
+    <ul className="hidden md:flex md:gap-2">
+      <li>
+        <Link href="/">
+          <a className="text-lg font-medium">Home</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/portfolio">
+          <a className="text-lg font-medium">Portfolio</a>
+        </Link>
+      </li>
+      <li>
+        <Link href="/contact">
+          <a className="text-lg font-medium">Contact</a>
+        </Link>
+      </li>
+    </ul>
+  );
+}
+
+function MobileNav({ setToggleNav }) {
+  return (
+    <>
+      <div className="absolute inset-0 bg-black bg-opacity-40" />
+      <div className="fixed inset-y-0 w-[80%] bg-[#1B1B22] right-0 text-white p-5">
+        <div className="flex justify-between mb-10">
+          <h1 className="text-2xl font-semibold">Menu</h1>
+          <button
+            onClick={() => setToggleNav((prev) => !prev)}
+            className="p-2 bg-white text-[#DD1155] text-lg rounded font-medium"
+          >
+            Close Menu
+          </button>
+        </div>
+        <nav>
+          <ul className="flex flex-col gap-5">
+            <li>
+              <Link href="/">
+                <a className="block py-3 text-2xl font-medium">Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/portfolio">
+                <a className="block py-3 text-2xl font-medium">Portfolio</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact">
+                <a className="block py-3 text-2xl font-medium">Contact</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </>
   );
 }
 
 function Hero() {
   return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <div className="flex flex-col items-center gap-28 w-full">
-        <div className="w-52 h-52 rounded-full bg-white" />
-        <p className="text-white text-center text-2xl font-bold">
-          Hello, my name is david and I am a freelance web developer. I aim to
-          provide meaningful value to your business.
+    <div className="flex items-center justify-center w-full h-screen">
+      <div className="flex flex-col items-center w-full gap-28">
+        <div className="border-8 border-transparent rounded-full w-52 h-52 border-y-slate-900 animate-spinLeft" />
+        <div className="absolute mt-4 border-8 border-transparent rounded-full w-44 h-44 border-x-slate-800 animate-spinRight" />
+        <div className="absolute mt-8 bg-white rounded-full w-36 h-36" />
+
+        <p className="text-2xl font-bold text-center text-white">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores
+          quisquam omnis ex autem.
         </p>
       </div>
     </div>
@@ -58,31 +133,34 @@ function Portfolio() {
     <div className="bg-[#1B1B22] text-white">
       <h2 className="mb-2 text-2xl">Portfolio</h2>
       <div className="flex flex-col gap-2">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+        <Card tag="Personal Project" />
+        <Card tag="Client Project" />
+        <Card tag="Client Project" />
+        <Card tag="Concept" />
         <Card />
       </div>
     </div>
   );
 }
 
-function Card() {
+function Card({ tag = 'Personal Project' }) {
   return (
     <div>
-      <div className="w-full h-72   bg-slate-600 rounded-t" />
-      <h3 className="bg-white text-black rounded-b py-2 text-lg">
+      <div className="w-full rounded-t h-72 bg-slate-600" />
+      <h3 className="p-2 text-lg text-black bg-white rounded-b">
         Project Name
       </h3>
+      <span className="block px-2 py-1 w-fit rounded relative bottom-80 left-3 text-left bg-white text-[#DD1155]">
+        {tag}
+      </span>
     </div>
   );
 }
 
 function Contact() {
   return (
-    <div className="text-white mt-5">
-      <h1 className="text-2xl mb-2">Need to Contact Me?</h1>
+    <div className="mt-5 text-white">
+      <h1 className="mb-2 text-2xl">Need to Contact Me?</h1>
       <p className="text-lg font-base">
         If you need to contact me, you can get to me at my email
         <a
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,84 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      keyframes: {
+        spinL: {
+          '0%': {
+            transform: 'rotate(0deg)',
+          },
+          '10%': {
+            transform: 'rotate(-36deg)',
+          },
+          '20%': {
+            transform: 'rotate(-72deg)',
+          },
+          '30%': {
+            transform: 'rotate(-108deg)',
+          },
+          '40%': {
+            transform: 'rotate(-144deg)',
+          },
+          '50%': {
+            transform: 'rotate(-180deg)',
+          },
+          '60%': {
+            transform: 'rotate(-216deg)',
+          },
+          '70%': {
+            transform: 'rotate(-252deg)',
+          },
+          '80%': {
+            transform: 'rotate(-288deg)',
+          },
+          '90%': {
+            transform: 'rotate(-324deg)',
+          },
+          '100%': {
+            transform: 'rotate(-360deg)',
+          },
+        },
+        spinR: {
+          '0%': {
+            transform: 'rotate(0deg)',
+          },
+          '10%': {
+            transform: 'rotate(36deg)',
+          },
+          '20%': {
+            transform: 'rotate(72deg)',
+          },
+          '30%': {
+            transform: 'rotate(108deg)',
+          },
+          '40%': {
+            transform: 'rotate(144deg)',
+          },
+          '50%': {
+            transform: 'rotate(180deg)',
+          },
+          '60%': {
+            transform: 'rotate(216deg)',
+          },
+          '70%': {
+            transform: 'rotate(252deg)',
+          },
+          '80%': {
+            transform: 'rotate(288deg)',
+          },
+          '90%': {
+            transform: 'rotate(324deg)',
+          },
+          '100%': {
+            transform: 'rotate(360deg)',
+          },
+        },
+      },
+      animation: {
+        spinLeft: 'spinL 5s linear infinite',
+        spinRight: 'spinR 5s linear infinite',
+      },
+    },
+    plugins: [],
   },
-  plugins: [],
 };
